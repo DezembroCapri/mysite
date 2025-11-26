86aeacb4e2b4bb04edeea3efad50473a625abbce
DezembroCapri <oolavio49@gmail.com>
2025-11-26 19:15:30 +0000

    Add latest blog posts to homepage

--- home/models.py
+++ home/models.py
@@ -8,4 +8,13 @@ from wagtail.admin.panels import FieldPanel
 class HomePage(Page):
     body = RichTextField(blank=True)
 
-    content_panels = Page.content_panels + [FieldPanel("body"),]
+    content_panels = Page.content_panels + [
+        FieldPanel("body"),
+    ]
+
+    def get_context(self, request):
+        context = super().get_context(request)
+        from blog.models import BlogPage
+        context["latest_posts"] = BlogPage.objects.live().order_by("-date")[:5]
+        return context
+
